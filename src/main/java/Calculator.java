cdff8b31d33ffa582eb1183a5cd93552fc5d6791
Alexander <cheremiskinalexander@yandex.ru>
2021-03-26 20:53:43 +0300

    add variables + add exceptions

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -5,7 +5,10 @@ public class Calculator {
     private static StringBuilder exp = new StringBuilder();
     private static Stack<Double> values = new Stack<>();
     private static Stack<Character> operators = new Stack<>();
-    public static final Map<Character, Integer> priorities = new HashMap<>();
+
+    private static final Map<Character, Integer> priorities = new HashMap<>();
+    private static Map<Character, Double> variables = new HashMap<>();
+
     static {
         priorities.put('(', -1);
         priorities.put(')', 100);
@@ -15,27 +18,62 @@ public class Calculator {
         priorities.put('/', 1);
     }
 
-    private static void readNextToken() {
+    private static void readNextToken() throws Exception {
         char startToken = exp.charAt(0);
 
         int endTokenPos = 1;
         boolean wasDot = false;
 
         if (isOperator(startToken)) {
+            while (!operators.empty() && priorities.get(startToken) > priorities.get(operators.peek())) {
+                if (operators.peek() != '(') {
+                    double v2 = values.pop();
+                    double v1 = values.pop();
+                    values.push(executeOperation(v1, v2, operators.pop()));
+                } else {
+                    operators.pop();
+                }
+            }
             operators.push(startToken);
         } else if (isDigit(startToken)) {
             while (isDigit(exp.charAt(endTokenPos)) || exp.charAt(endTokenPos) == '.' && !wasDot) {
                 if (exp.charAt(endTokenPos) == '.') wasDot = true;
                 endTokenPos++;
             }
-
             values.push(Double.parseDouble(exp.substring(0, endTokenPos)));
         }
-
+        else if (isVariable(startToken)) {
+            if (variables.containsKey(startToken)) {
+                values.push(variables.get(startToken));
+            } else {
+                System.out.println("Enter '"+ startToken + "': ");
+                Scanner in = new Scanner(System.in);
+                double num = Double.parseDouble(in.nextLine());
+                variables.put(startToken, num);
+                values.push(num);
+            }
+        } else {
+            throw new Exception("Unknown symbol:" + startToken);
+        }
         exp.delete(0, endTokenPos);
     }
 
-    public static double calculate(String expression) {
+    private static double executeOperation(double v1, double v2, char operator) throws Exception {
+        switch (operator) {
+            case '+':
+                return v1 + v2;
+            case '-':
+                return v1 - v2;
+            case '*':
+                return v1 * v2;
+            case '/':
+                return v1 / v2;
+            default:
+                throw new Exception("Unknown operation");
+        }
+    }
+
+    public static double calculate(String expression) throws Exception {
         exp = new StringBuilder(expression);
 
         exp.insert(0, '(');
@@ -45,10 +83,11 @@ public class Calculator {
             readNextToken();
         }
 
-        System.out.println(values);
-        System.out.println(operators);
+        if (values.size() > 1 || operators.size() > 0) {
+            throw new Exception("Unable to calculate expression");
+        }
 
-        return 1.1;
+        return values.pop();
     }
 
     private static boolean isOperator(char symbol) {
@@ -57,5 +96,9 @@ public class Calculator {
 
     private static boolean isDigit(char symbol) {
         return symbol >= '0' && symbol <= '9';
+    }
+
+    private static boolean isVariable(char symbol) {
+        return symbol >= 'a' && symbol <= 'z' || symbol >= 'A' && symbol <='Z';
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
-        Calculator.calculate("2.8 + 489.999");
+    public static void main(String[] args)  {
+        try {
+            System.out.println(Calculator.calculate("x + y * x"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
